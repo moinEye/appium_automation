99376a27e4757587055017f5ceeeca1d7428bc9c
moinuahmed <moinuahmed2011@gmail.com>
2022-12-06 16:13:37 +0600

    fix:Methods

---
+++ AndroidDrivers/DriverMethods.py
@@ -0,0 +1,26 @@
+from appium import webdriver
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps ['automationName'] = 'UiAutomator2'
+desired_caps['platformVersion'] = '11'
+desired_caps['deviceName'] = 'samsung SM-M015G'
+desired_caps['app'] = ('/home/moin/Downloads/Appium/Android_Demo_App.apk')
+desired_caps['appPackage'] = 'com.code2lead.kwad'
+desired_caps['appActivity'] = 'com.code2lead.kwad.MainActivity'
+
+# Part 2 "WebDriver object"
+
+driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+
+print("Check if device is locked or not :", driver.is_locked())
+print("Current Package :", driver.current_package)
+print("Current Activity:", driver.current_activity)
+print("Current Context:", driver.orientation)
+
+
+
+
+
+
+
