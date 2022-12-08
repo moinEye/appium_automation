61b287649196f47d885c2a1eb64e0757c136b64f
moinuahmed <moinuahmed2011@gmail.com>
2022-12-08 16:21:22 +0600

    fix:scroll

---
+++ Gestures/ScrollViewEx.py
@@ -0,0 +1,43 @@
+from datetime import time
+
+from appium import webdriver
+from appium.webdriver import appium_service
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.common import ElementNotVisibleException, ElementNotSelectableException, NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+
+import time
+
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps ['automationName'] = 'UiAutomator2'
+desired_caps['platformVersion'] = '11'
+desired_caps['deviceName'] = 'samsung SM-M015G'
+desired_caps['app'] = ('/home/moin/Downloads/Appium/Android_Demo_App.apk')
+desired_caps['appPackage'] = 'com.code2lead.kwad'
+desired_caps['appActivity'] = 'com.code2lead.kwad.MainActivity'
+
+driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+
+wait = WebDriverWait(driver, 25, poll_frequency=1,
+                     ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException,
+                                         NoSuchElementException])
+
+ele = wait.until(lambda x: x.find_element(AppiumBy.ID, "com.code2lead.kwad:id/ScrollView"))
+ele.click()
+
+ele = wait.until(lambda x: x.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
+                                          'new UiScrollable(new UiSelector()).scrollIntoView(text("BUTTON12"))'))
+ele.click()
+
+time.sleep(4)
+driver.quit()
+
+
+
+
+
+
+
+
+
